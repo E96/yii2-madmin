8ef8a766fcf00fba2ed6624258de5a7c2e64b718
m8rge <to.merge@gmail.com>
2015-01-28 15:51:26 +0500

    init twig

--- src/Module.php
+++ src/Module.php
@@ -5,6 +5,7 @@ namespace e96\madmin;
 
 use yii\base\Application;
 use yii\base\BootstrapInterface;
+use yii\twig\ViewRenderer;
 
 class Module extends \yii\base\Module implements BootstrapInterface
 {
@@ -15,5 +16,30 @@ class Module extends \yii\base\Module implements BootstrapInterface
     public function bootstrap($app)
     {
         \Yii::setAlias('madmin', __DIR__);
+
+        if (is_null(\Yii::$app->view->renderers) || !array_key_exists('twig', \Yii::$app->view->renderers)) {
+            \Yii::$app->view->renderers['twig'] = [
+                'class' => ViewRenderer::className(),
+                'cachePath' => '@runtime/Twig/cache',
+                'globals' => [
+                    'html' => '\yii\helpers\Html'
+                ],
+                'options' => [
+                    'auto_reload' => true,
+                ],
+                'uses' => [
+                    'yii\bootstrap',
+                    'yii\grid'
+                ],
+            ];
+        } else {
+            \Yii::$app->view->renderers['twig']['globals']['html'] = '\yii\helpers\Html';
+            if (!array_search('yii\bootstrap', \Yii::$app->view->renderers['twig']['uses'])) {
+                \Yii::$app->view->renderers['twig']['uses'][] = 'yii\bootstrap';
+            }
+            if (!array_search('yii\grid', \Yii::$app->view->renderers['twig']['uses'])) {
+                \Yii::$app->view->renderers['twig']['uses'][] = 'yii\grid';
+            }
+        }
     }
 }
